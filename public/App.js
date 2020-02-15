7006fd41e1c5e6b2e2691309d3259cf9bf00dcf0
sahithiartala <60410833+sahithiartala@users.noreply.github.com>
2020-02-14 21:43:39 -0800

    Building jsx

---
+++ public/App.js
@@ -0,0 +1,128 @@
+const productInventory = [];
+
+class ProductList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: []
+    };
+    this.createProduct = this.createProduct.bind(this);
+  }
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    setTimeout(() => {
+      this.setState({
+        products: productInventory
+      });
+    }, 500);
+  }
+
+  createProduct(product) {
+    product.id = this.state.products.length + 1;
+    const newProductList = this.state.products.slice();
+    newProductList.push(product);
+    this.setState({
+      products: newProductList
+    });
+  }
+
+  render() {
+    return React.createElement(React.Fragment, null, React.createElement("h2", null, "My Company Inventory"), React.createElement("h4", null, " Showing all the available products"), React.createElement("hr", null), React.createElement(ProductTable, {
+      products: this.state.products
+    }), React.createElement("h4", null, "Add a new product to inventory"), React.createElement("hr", null), React.createElement(ProductAdd, {
+      createProduct: this.createProduct
+    }));
+  }
+
+}
+
+function ProductTable(props) {
+  {
+    const productRows = props.products.map(product => React.createElement(ProductRow, {
+      key: product.id,
+      product: product
+    }));
+    return React.createElement("table", {
+      className: "bordered-table"
+    }, React.createElement("thead", null, React.createElement("tr", null, React.createElement("th", null, "Product Name"), React.createElement("th", null, "Price"), React.createElement("th", null, "Category"), React.createElement("th", null, "Image"))), React.createElement("tbody", null, productRows));
+  }
+}
+
+function ProductRow(props) {
+  {
+    const product = props.product;
+    return React.createElement("tr", null, React.createElement("td", null, product.name), React.createElement("td", null, product.category), React.createElement("td", null, product.Price), React.createElement("td", null, React.createElement("a", {
+      href: product.Image,
+      target: "_blank"
+    }, "View")));
+  }
+}
+
+class ProductAdd extends React.Component {
+  constructor() {
+    super();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = document.forms.productAdd;
+    const product = {
+      name: form.Product_Name.value,
+      Price: form.Price.value,
+      Image: form.Image_URL.value,
+      category: form.category.value
+    };
+    this.props.createProduct(product);
+    form.Product_Name.value = "";
+    form.Price.value = "", form.Image_URL.value = "";
+    form.category.value = "";
+  }
+
+  render() {
+    return React.createElement("form", {
+      name: "productAdd",
+      onSubmit: this.handleSubmit
+    }, React.createElement("div", {
+      className: "wrapper_1"
+    }, React.createElement("label", null, " Product Name:"), React.createElement("br", null), React.createElement("input", {
+      type: "text",
+      name: "Product_Name",
+      placeholder: "Product Name"
+    }), React.createElement("br", null), React.createElement("label", null, "Price Per Unit "), " ", React.createElement("br", null), React.createElement("input", {
+      type: "text",
+      name: "Price",
+      placeholder: "Price"
+    }), React.createElement("br", null), React.createElement("button", {
+      type: "submit"
+    }, "Add Product")), React.createElement("div", {
+      className: "wrapper_2"
+    }, React.createElement("label", null, "Category"), React.createElement("br", null), React.createElement("select", {
+      name: "category"
+    }, React.createElement("option", {
+      value: ""
+    }, "Select your Category"), React.createElement("option", {
+      value: "Shirts"
+    }, "Shirts"), React.createElement("option", {
+      value: "Jeans"
+    }, "Jeans"), React.createElement("option", {
+      value: "Jackets"
+    }, "Jackets"), React.createElement("option", {
+      value: "Sweaters"
+    }, "Sweaters"), React.createElement("option", {
+      value: "Accessories"
+    }, "Accessories")), " ", React.createElement("br", null), React.createElement("label", null, "Image_URL"), React.createElement("br", null), React.createElement("input", {
+      type: "text",
+      name: "Image_URL",
+      placeholder: "URL"
+    }), React.createElement("br", null)));
+  }
+
+}
+
+const element = React.createElement(ProductList, null);
+ReactDOM.render(element, document.getElementById('contents'));
---
+++ src/App.jsx
@@ -0,0 +1,136 @@
+const productInventory=[];
+
+class ProductList extends React.Component {
+  constructor() {
+  super();
+  this.state = { products: [] };
+  this.createProduct = this.createProduct.bind(this);
+  }
+  componentDidMount() {
+    this.loadData();
+  }
+  loadData() {
+    setTimeout(() => {
+      this.setState({ products: productInventory });
+    }, 500);
+   
+  }
+createProduct(product) {
+  product.id= this.state.products.length + 1;
+  const newProductList = this.state.products.slice();
+  newProductList.push(product);
+  this.setState({ products: newProductList });
+}
+  render() {
+    return (
+      <React.Fragment>
+        <h2>My Company Inventory</h2>
+        <h4> Showing all the available products</h4>
+        <hr />
+        <ProductTable  products={this.state.products}/>
+        <h4>Add a new product to inventory</h4>
+        <hr />
+        <ProductAdd createProduct={this.createProduct}/>
+      </React.Fragment>
+    );
+  }
+}
+
+
+function ProductTable(props) {    
+  {
+    const productRows = props.products.map(product =>
+      <ProductRow key={product.id} product={product} />
+    );
+      return (
+        <table className="bordered-table">
+          <thead>
+            <tr>
+             <th>Product Name</th>
+              <th>Price</th>
+              <th>Category</th>
+              <th>Image</th>
+            </tr>
+          </thead>
+          <tbody>
+            {productRows}
+          </tbody>
+        </table>
+      );
+    }
+  }
+
+  function ProductRow(props) {
+   {
+    const product = props.product;
+    return (
+      <tr>
+        
+        <td>{product.name}</td>
+        <td>{product.category}</td>
+        <td>{product.Price}</td>
+        <td><a href={product.Image} target="_blank">View</a></td>
+        
+      </tr>
+    );
+  }
+}
+
+class ProductAdd extends React.Component {
+  constructor() {
+    super();
+    
+    this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    handleSubmit(e) {
+      e.preventDefault();
+      const form = document.forms.productAdd;
+      const product = {
+        name: form.Product_Name.value,
+        Price: form.Price.value,
+         Image:form.Image_URL.value,
+         category:form.category.value
+      }
+      this.props.createProduct(product);
+      form.Product_Name.value = "";
+      form.Price.value = "",
+       form.Image_URL.value = "";
+       form.category.value = "";
+
+    }
+  render()
+   {
+    return (
+      
+      <form name="productAdd" onSubmit={this.handleSubmit}>
+            <div className="wrapper_1">
+            <label> Product Name:</label><br/>
+            <input type="text" name="Product_Name" placeholder="Product Name"  /><br/>
+            <label>Price Per Unit </label> <br/>
+            <input type="text" name="Price" placeholder="Price"  /><br/>
+            <button type="submit">Add Product</button>
+            </div>
+            <div className="wrapper_2">
+            <label>Category</label><br/>
+            <select name="category" >
+            <option value="">Select your Category</option>
+              <option value="Shirts">Shirts</option>
+              <option value="Jeans">Jeans</option>
+              <option value="Jackets">Jackets</option>
+              <option value="Sweaters">Sweaters</option>
+              <option value="Accessories">Accessories</option>
+            </select> <br />
+            <label>Image_URL</label><br/>
+            <input type="text" name="Image_URL"  placeholder="URL"  /><br/>
+            </div>
+            
+            </form>
+      
+    );
+  
+  }
+}
+
+const element = <ProductList />;
+
+ReactDOM.render(element, document.getElementById('contents'));
